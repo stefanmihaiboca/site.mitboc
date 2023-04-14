65132978ae7c448167875696e4b6df8054e482a2
Cezar Berea <cezar@dotts.ro>
2023-04-14 11:57:40 +0300

    Add database

--- server.js
+++ server.js
@@ -1,18 +1,54 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mariadb = require("mariadb");
+const app = express();
 
-app.set('view engine', 'ejs')
+const pool = mariadb.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "mitboc",
+});
 
-app.use(express.static(path.join(__dirname , 'public')))
+app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-  res.render('index')   
-})
-app.get('/produse', (req, res) => {
-res.render('produse')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.listen(3000)
- 
+app.get("/produse", (req, res) => {
+  res.render("produse");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.post("/contact", async (req, res) => {
+  const db = await pool.getConnection();
+  const body = req.body;
+  const query =
+    "INSERT INTO mesaje (nume, email, telefon, mesaj) VALUES(?, ?, ?, ?)";
+  const result = await db.query(query, [
+    body.nume,
+    body.email,
+    body.telefon,
+    body.mesaj,
+  ]);
+  res.render("contact");
+});
+
+app.get("/admin", async (req, res) => {
+  const db = await pool.getConnection();
+  const body = req.body;
+  const query = "SELECT * FROM mesaje";
+  const result = await db.query(query);
+  console.log(result);
+  res.render("admin", {messages: result});
+});
+
+app.listen(3000);
