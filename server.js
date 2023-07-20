a0ac7d1cede40292d3b00db288fc31c4adf0ea6a
stefanmihaiboca <bocastefanmihai@gmail.com>
2023-07-20 12:59:21 +0300

    admin.1

--- server.js
+++ server.js
@@ -16,43 +16,59 @@ const pool = mariadb.createPool({
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.render('index', { layout: 'layout' });
+app.get("/", (req, res) => {
+  res.render("index", { layout: "layout" });
 });
 
 app.get("/produse", (req, res) => {
-  res.render("produse", { layout: 'layout' });
+  res.render("produse", { layout: "layout" });
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact", { layout: 'layout' });
+  res.render("contact", { layout: "layout" });
 });
 
 app.post("/contact", async (req, res) => {
   const db = await pool.getConnection();
   const body = req.body;
   const query =
-    "INSERT INTO contact_tabela (nume, email, telefon, mesaj) VALUES(?, ?, ?, ?)";
+    "INSERT INTO contact_tabela (nume, email, telefon, mesaj) VALUES (?, ?, ?, ?)";
   const result = await db.query(query, [
     body.nume,
     body.email,
     body.telefon,
     body.mesaj,
   ]);
-  res.render("contact", { layout: 'layout' });
+  res.render("contact", { layout: "layout" });
 });
 
 app.get("/admin", async (req, res) => {
   const db = await pool.getConnection();
   const body = req.body;
-  const query = "SELECT * FROM mesaje";
+  const query = "SELECT * FROM contact_tabela";
   const result = await db.query(query);
   console.log(result);
-  res.render("admin", {messages: result,layout:'layout'});
+  res.render("admin", { layout: "layout" });
 });
-const adminRouter = require('./routes/admin');
-app.use('/admin', adminRouter);
+
+app.post("/admin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username === "Stefan" && password === "Stefan20") {
+    res.redirect("/table"); // Autentificare reușită, redirecționăm către pagina table.ejs
+  } else {
+    res.render("admin", { layout: "layout", error: "Autentificare eșuată! Verificați username-ul și parola introduse." });
+  }
+});
+
+app.get("/table", async (req, res) => {
+  const db = await pool.getConnection();
+  const query = "SELECT * FROM contact_tabela";
+  const result = await db.query(query);
+  console.log(result);
+  res.render("table", { layout: "layout", data: result });
+});
 
 app.listen(3000);
