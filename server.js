fa76bb9cfd9256b077df528548685b401399630c
stefanmihaiboca <bocastefanmihai@gmail.com>
2023-07-20 16:34:14 +0300

    ...

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ app.post("/contact", async (req, res) => {
 
 app.get("/admin", async (req, res) => {
   const db = await pool.getConnection();
-  const body = req.body;
   const query = "SELECT * FROM contact_tabela";
   const result = await db.query(query);
   console.log(result);
@@ -57,18 +56,42 @@ app.post("/admin", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   if (username === "Stefan" && password === "Stefan20") {
-    res.redirect("/table"); // Autentificare reușită, redirecționăm către pagina table.ejs
+    res.redirect("/table"); 
   } else {
     res.render("admin", { layout: "layout", error: "Autentificare eșuată! Verificați username-ul și parola introduse." });
   }
 });
-
 app.get("/table", async (req, res) => {
   const db = await pool.getConnection();
   const query = "SELECT * FROM contact_tabela";
   const result = await db.query(query);
   console.log(result);
   res.render("table", { layout: "layout", data: result });
+});
+app.delete("/table/delete/:id", async (req, res) => {
+  try {
+    const messageId = req.params.id; // Folosim "id" cu literă mică
+    const db = await pool.getConnection();
+    const query = "DELETE FROM contact_tabela WHERE ID = ?"; // Folosim "ID" cu literă mare
+    await db.query(query, [messageId]);
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ success: false });
+  }
+});
+
+app.put("/table/mark/:id", async (req, res) => {
+  try {
+    const messageId = req.params.id; // Folosim "id" cu literă mică
+    const db = await pool.getConnection();
+    const query = "UPDATE contact_tabela SET marked = 1 WHERE id = ?"; // Folosim "id" cu literă mică
+    await db.query(query, [messageId]);
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ success: false });
+  }
 });
 
 app.listen(3000);
